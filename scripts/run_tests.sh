acf96ed9bb829793815a0369f3b8ff31cd4d01eb
Matthew <matthewbrown595@gmail.com>
2024-03-28 14:30:26 +0000

    added server tests

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,1 +1,26 @@
-sudo npm test
+#!/bin/bash
+
+# Change directory to where your application is deployed
+cd /var/www/html/myapp
+
+# Run linting tests
+npm run lint
+
+# Run unit tests
+npm run test:unit
+
+# Run integration tests
+npm run test:integration
+
+# Capture the exit codes of the tests
+lint_exit=$?
+unit_exit=$?
+integration_exit=$?
+
+# Exit with appropriate exit code based on test results
+# For example, exit with code 1 if any of the tests fail, and 0 if all tests pass
+if [ $lint_exit -ne 0 ] || [ $unit_exit -ne 0 ] || [ $integration_exit -ne 0 ]; then
+  exit 1
+fi
+
+exit 0
