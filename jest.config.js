c2c844440927de68d460c0e8d350ff8abbb27f14
Matthew <matthewbrown595@gmail.com>
2024-03-28 15:42:29 +0000

    increased timeout

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@
 module.exports = {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
-
+  testTimeout: 100000,
   // Stop running tests after `n` failures
   // bail: 0,
 
